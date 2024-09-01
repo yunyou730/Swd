5fad35b617ef31aefab23312b03ed6d8a8036810
yunyou <502765299@qq.com>
2024-09-01 10:04:21 +0800

    +Addressable 的一些尝试

--- Swd/Assets/Scripts/ResManager/LoadWithLabel.cs
+++ Swd/Assets/Scripts/ResManager/LoadWithLabel.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class LoadWithLabel : MonoBehaviour
 {
+    [SerializeField]
+    private AssetReference _test;
+    [SerializeField]
+    private AssetReferenceGameObject _test2;
+    [SerializeField]
+    private AssetReferenceT<GameObject> _test3;
+
     // Start is called before the first frame update
     void Start()
     {
