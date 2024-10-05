276d0ec693d1764c213dd9cefd3ccee614d92128
yunyou <502765299@qq.com>
2024-10-05 19:55:34 +0800

    +增加 InGameConsole 用于测试

--- Swd/Assets/Scripts/ClashGame/ClashGame.cs
+++ Swd/Assets/Scripts/ClashGame/ClashGame.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using clash.gameplay;
+using IngameDebugConsole;
 using LitJson;
 using swd;
 using UnityEngine;
@@ -45,5 +46,12 @@ namespace clash
         {
             return _resManager;
         }
+        
+        [ConsoleMethod( "cube", "Creates a cube at specified position" )]
+        public static void CreateCubeAt( Vector3 position )
+        {
+            GameObject.CreatePrimitive( PrimitiveType.Cube ).transform.position = position;
+        }
+        
     }
 }
