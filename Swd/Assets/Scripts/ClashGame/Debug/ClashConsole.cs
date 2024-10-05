a393f4cddc011b0efc757041c0d9b914cd59e676
yunyou <502765299@qq.com>
2024-10-05 19:59:41 +0800

    +增加 Clash Console 专门用于拓展 In Game Console

---
+++ Swd/Assets/Scripts/ClashGame/Debug/ClashConsole.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using IngameDebugConsole;
+
+namespace clash.debug
+{
+    public class ClashConsole
+    {
+        [ConsoleMethod( "ClashGameTest", "ClashGameTest" )]
+        public static void CreateCubeAt( Vector3 position )
+        {
+            Debug.Log("test1");
+            GameObject.CreatePrimitive( PrimitiveType.Cube ).transform.position = position;
+        }
+    }
+}
