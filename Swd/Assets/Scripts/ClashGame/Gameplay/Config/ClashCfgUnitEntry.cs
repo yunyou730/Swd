0d93c793c5aabee01312817a59c6aaa26b37b5cd
yunyou <502765299@qq.com>
2024-10-06 13:09:34 +0800

    +增加一个 基础单位的 fbx

---
+++ Swd/Assets/Scripts/ClashGame/Gameplay/Config/ClashCfgUnitEntry.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+namespace clash.gameplay
+{
+    [Serializable,CreateAssetMenu(fileName = "ClashConfigUnitEntry", menuName = "ClashGame/ScriptableObjects/ClashConfigUnitEntry")]
+    public class ClashCfgUnitEntry : ScriptableObject
+    {
+        public string Tag;
+        public string PrefabPath;
+        public float MovementSpeed;
+    }
+}
---
+++ Swd/Assets/Scripts/ClashGame/Gameplay/Config/ClashCfgUnits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace clash.gameplay
+{
+    [Serializable,CreateAssetMenu(fileName = "ClashConfigUnits", menuName = "ClashGame/ScriptableObjects/ClashConfigUnits")]
+    public class ClashCfgUnits : ScriptableObject
+    {
+        public Dictionary<string, string> units;
+    }
+}
