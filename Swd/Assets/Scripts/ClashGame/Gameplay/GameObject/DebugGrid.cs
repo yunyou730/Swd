752c1fec6df5e0f5d97156fe628a4bbdb12915ac
yunyou <502765299@qq.com>
2024-10-09 23:19:03 +0800

    +DebugGrid 用 随机生成的贴图 来传递 数据到 material 里面

--- Swd/Assets/Scripts/ClashGame/Gameplay/GameObject/DebugGrid.cs
+++ Swd/Assets/Scripts/ClashGame/Gameplay/GameObject/DebugGrid.cs
@@ -2,6 +2,7 @@
 using clash.gameplay.Utilities;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = System.Random;
 
 
 namespace clash.gameplay.GameObject
@@ -11,6 +12,11 @@ namespace clash.gameplay.GameObject
         private UnityEngine.GameObject _parentGameObject = null;
         private UnityEngine.GameObject _go = null;
         private ClashWorld _world = null;
+        
+        private Texture2D _tileDataTex = null;
+        private static readonly int kGridWidth = Shader.PropertyToID("_GridWidth");
+        private static readonly int kGridHeight = Shader.PropertyToID("_GridHeight");
+        private static readonly int kTileDataTex = Shader.PropertyToID("_TileDataTex");
 
         public DebugGrid(ClashWorld world,UnityEngine.GameObject parentGameObject)
         {
@@ -29,8 +35,15 @@ namespace clash.gameplay.GameObject
             // Mesh
             MeshFilter meshFilter = _go.AddComponent<MeshFilter>();
             meshFilter.mesh = mesh;
+            
+            // data texture
+            _tileDataTex = GenerateTexture(gameStartMeta.GridWidth,gameStartMeta.GridHeight);
 
             // material
+            material.SetFloat(kGridWidth,gameStartMeta.GridWidth);
+            material.SetFloat(kGridHeight,gameStartMeta.GridHeight);
+            material.SetTexture(kTileDataTex,_tileDataTex);
+
             MeshRenderer meshRenderer = _go.AddComponent<MeshRenderer>();
             //meshRenderer.material = new Material(Shader.Find("Standard"));
             meshRenderer.material = material;
@@ -51,6 +64,7 @@ namespace clash.gameplay.GameObject
             Vector3[] vertices = new Vector3[width * height * 4];
             int[] triangles = new int[width * height * 6];
             Vector2[] uvs = new Vector2[width * height * 4];
+            Vector2[] tileCoord = new Vector2[width * height * 4];
 
             
             int vertIndex = 0;
@@ -61,16 +75,24 @@ namespace clash.gameplay.GameObject
                 {
                     Vector3 center = ClashUtility.GetPositionAtTile(_world,x, z);
 
+                    // semantic position
                     vertices[vertIndex] = center + new Vector3(-1, 0, -1) * clashConfig.TileSize * 0.5f;
                     vertices[vertIndex + 1] = center + new Vector3(1, 0, -1) * clashConfig.TileSize * 0.5f;
                     vertices[vertIndex + 2] = center + new Vector3(1, 0, 1) * clashConfig.TileSize * 0.5f;
                     vertices[vertIndex + 3] = center + new Vector3(-1, 0, 1) * clashConfig.TileSize * 0.5f;
 
+                    // semantic UV
                     uvs[vertIndex] = new Vector2(0, 0);
                     uvs[vertIndex + 1] = new Vector2(1, 0);
                     uvs[vertIndex + 2] = new Vector2(1, 1);
                     uvs[vertIndex + 3] = new Vector2(0, 1);
+
+                    tileCoord[vertIndex] = new Vector2(x,z);
+                    tileCoord[vertIndex + 1] = new Vector2(x,z);
+                    tileCoord[vertIndex + 2] = new Vector2(x,z);
+                    tileCoord[vertIndex + 3] = new Vector2(x,z);
                     
+                    // face
                     triangles[faceIndex++] = vertIndex + 0;
                     triangles[faceIndex++] = vertIndex + 2;
                     triangles[faceIndex++] = vertIndex + 1;
@@ -82,18 +104,46 @@ namespace clash.gameplay.GameObject
                 }
             }
             
-
             mesh.vertices = vertices;
             mesh.triangles = triangles;
             mesh.uv = uvs;
+            mesh.uv2 = tileCoord;
             mesh.RecalculateNormals();
 
             return mesh;
         }
 
+        private Texture2D GenerateTexture(int width,int height)
+        {
+            Texture2D tex = new Texture2D(width,height,TextureFormat.RGB24,false);
+            tex.filterMode = FilterMode.Point;
+            tex.wrapMode = TextureWrapMode.Clamp;
+
+            Random random = new Random();
+
+            // here we should fill with data. @miao @todo
+            Color[] colors = new Color[width * height];
+            for (int x = 0;x < width;x++)
+            {
+                for (int y = 0;y < height;y++)
+                {
+                    int randomInRange = random.Next(1,10);
+                    Color col = randomInRange < 5 ? Color.black : Color.white;
+                    colors[y * width + x] = col;
+                }
+            }
+            tex.SetPixels(colors);
+            tex.Apply();
+
+            return tex;
+        }
+
         public void Dispose()
         {
-            UnityEngine.GameObject.Destroy(_go);
+            UnityEngine.Object.Destroy(_tileDataTex);
+            _tileDataTex = null;
+            
+            UnityEngine.Object.Destroy(_go);
             _go = null;
         }
     }
